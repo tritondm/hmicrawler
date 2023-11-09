af2a299d383eadea0215ed2e0795752ac1c6c686
tritondm <andreas.kraxner@gmail.com>
2023-11-09 10:54:17 +0100

    update comments

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # hmicrawler
-SIMATIC HMI Miniweb Crawler
+## SIMATIC HMI Miniweb Crawler
+
+2023.05.12 - by andreas.kraxner@hblfa-tirol.at
+this script gets all json files from a siemens hmi
+the json files stored on the SDCARD see storage configuration
+example:
+```
+    python3 hmicrawler.py --url 'https://192.168.10.200:443' --dest '/tmp/fish' --backup --user '#youruser#' --password '#mypwd#'
+```
--- hmicrawler.py
+++ hmicrawler.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # coding: utf8
-# 2023.05.12 - by andreas.kraxner@hblfa-tirol.at
-
+"""
+2023.05.12
+this script gets all json files from a siemens hmi
+the json files stored on the SDCARD see storage configuration
+example:
+    python3 hmicrawler.py --url 'https://192.168.10.200:443' --dest '/tmp/fish' --backup --user '#youruser#' --password '#mypwd#'
+"""
 import requests
 
 #disable warning by change settings in the urllib3
@@ -12,6 +17,15 @@ import os
 import datetime
 import json
 from urllib.parse import quote
+
+__author__ = "Andreas Kraxner"
+__copyright__ = "Copyright 2023, tritondm"
+__credits__ = [" Andreas Kraxner"]
+__license__ = "GPLv3"
+__version__ = "1.0.1"
+__maintainer__ = "Andreas Kraxner"
+__email__ = "andreas.kraxner@hblfa-tirol.at"
+__status__ = "Production"
 
 snitoken='%=Token'
 
